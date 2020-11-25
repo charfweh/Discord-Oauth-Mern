c4f3363745f7d01def86932ffe59d6a056c48014
charfweh <affan.ismail2016@gmail.com>
2020-11-25 20:20:14 +0530

    finna getting started

--- clientside/src/App.js
+++ clientside/src/App.js
@@ -2,13 +2,33 @@ import './App.css';
 import React from 'react';
 import ButtonComponent from "./Button"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from "@material-ui/core/Container"
+import KeyboardArrowRightOutlinedIcon from '@material-ui/icons/KeyboardArrowRightOutlined';
 class App extends React.Component {
+  
+  // state
+  constructor(props){
+    super(props);
+    this.count = 0
+    this.state = {
+      count: this.count
+    }
+  }
+  setCount = ()=>{
+    this.count = this.count + 1
+    this.setState({count:this.count})
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <p>Iuhhh</p>
-          <ButtonComponent name = "Login"/>
+          <h1><KeyboardArrowRightOutlinedIcon fontSize = "large"/>GuildBoard</h1>
+          <Container className = "infoContainer" maxWidth = "lg">
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+          when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          </Container>
+          <ButtonComponent name = "Login" onClick = {this.handleState} color = "primary"/>
         </div>
       </div>
     );
--- clientside/src/Button.js
+++ clientside/src/Button.js
@@ -1,9 +1,19 @@
 import React from "react"
-import Button from "react-bootstrap/Button"
+import Button from  "@material-ui/core/Button"
+import axios from "axios"
+
 class ButtonComponent extends React.Component{
+    authHanlder = ()=>{
+        axios.get("/login")
+        .then(res=>{
+            console.log(res.data,res.status)
+        }).catch(err=>{
+            console.error((err))
+        })
+      }
     render(){
         return(
-            <Button variant = "outline-primary" size = "lg">{this.props.name}</Button>
+            <Button variant="contained" onClick = {this.authHanlder} color = "primary">{this.props.name}</Button>
         )
     }
 }
