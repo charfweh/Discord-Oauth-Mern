2ecc30888dcf92c7e380499198069aaaee4f906d
charfweh <affan.ismail2016@gmail.com>
2020-12-07 11:14:34 +0530

    Update: removed obscure functions

--- server/bot/logHandlerBot.js
+++ server/bot/logHandlerBot.js
@@ -1,69 +1,13 @@
 const discord = require("discord.js")
 const bot = new discord.Client()
-const {token, bot_prefix, webhookid, webhooktoken} = require("../config.json")
+const {token, bot_prefix} = require("../config.json")
+
 bot.on("ready",()=>{
   console.log("im ready")
 })
 function userhit(userResponse){
   //this channel id is of the server im in, please replace with channel id of your server
   bot.channels.cache.get('730731531428954172').send(`User authorize with ${userResponse}`)
-}
-function checkguild(member_id,guilds){
-  owner_guilds = []
-  bot_guilds = []
-  invitebot_guilds = []
-  guilds.forEach((item, i) => {
-    if(item.owner == true){
-      owner_guilds.push({"name":item.name,"id":item.id})
-      if(bot.guilds.cache.get(item.id)){
-        bot_guilds.push({"name":item.name,"id":item.id})
-      }
-      if(!bot.guilds.cache.get(item.id)){
-        invitebot_guilds.push({"name":item.name,"id":item.id})
-      }
-    }
-  });
-  return {
-    bot_guilds,
-    invitebot_guilds
-  }
-}
-
-// return info for GUILD_ID
-function manageguild(g_id){
-  let guild = bot.guilds.cache.get(g_id)
-  if(guild){
-    let memberCount = guild.memberCount
-    let guildname = guild.name
-    let iconurl = guild.iconURL()
-    let id = guild.id
-    return {
-      memberCount,
-      guildname,
-      iconurl,
-      id
-    }
-  }
-  return "ERR_INVALID_GUILD"
-}
-
-//parse the channels from POST request
-async function modifyguild(channels, g_id, webuser){
-    let c = channels[0].split(',')
-    editguild = bot.guilds.cache.get(g_id)
-    if(!editguild.channels.cache.find(c=>c.name == 'dashboard-logs')) return "Error"
-    else{
-      let logs = await editguild.channels.cache.find(c => c.name == 'dashboard-logs')
-      logs.send(`User initiated edit guild ${webuser.tag}`)
-      c.forEach((item, i) => {
-        editguild.channels.create(item,"text").then(()=>{
-          console.log("Channel created",item)
-        }).catch((e)=>{
-          editguild.channels.cache.find(c => c.name == 'dashboard-logs').send(`[-] Error: ${e}`)
-        })
-      });
-    }
-    return "OK"
 }
 
 //setup function for dashboard logging
@@ -79,10 +23,6 @@ bot.on("message",async message=>{
       })
     }else return message.channel.send("[-] You do not have correct permission to run this command")
   }
-  if(message.channel.name == 'irc'){
-  	console.log(message.content)
-    
-  }
 })
 
 bot.on("guildCreate",async()=>{
@@ -91,4 +31,4 @@ bot.on("guildCreate",async()=>{
 
 //i'd def want something nice?
 bot.login(token)
-module.exports =  { userhit, manageguild, checkguild, modifyguild};
+module.exports =  { userhit };
