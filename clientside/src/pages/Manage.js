168ecdc83bd77b7aed2710be90a464d0f8b15999
charfweh <affan.ismail2016@gmail.com>
2021-01-16 12:15:00 +0530

    Manage page rework

--- clientside/src/pages/Manage.js
+++ clientside/src/pages/Manage.js
@@ -9,7 +9,8 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { useParams } from "react-router-dom";
-
+import axios from 'axios';
+let mutualGuilds, inviteGuilds = [] 
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
@@ -18,14 +19,27 @@ const useStyles = makeStyles({
     height: 140,
   },
 });
-
+  axios.get("/manage/getGuildData").then(res=>{
+    console.log(res.data)
+    mutualGuilds = res.data.mutualGuilds
+    inviteGuilds = res.data.inviteGuilds
+    console.log(mutualGuilds[0])
+  }).catch(err=>{
+    console.log(err)
+  })
 export default function Manage() {
     const classes = useStyles();
     const id = useParams();
     console.log(id)
-  return (
+  
+    {mutualGuilds.map((name,id)=>{
+      console.log(name,id)
+    })}
+    return (
       <div className = "App">
       <NavBarComponent/>
+    
+    
     <Card className={classes.root}>
       <CardActionArea>
         <CardMedia
@@ -35,10 +49,10 @@ export default function Manage() {
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Server Name 
+            {mutualGuilds[0].name}
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-              Server details here
+            {mutualGuilds[0].id}
           </Typography>
         </CardContent>
       </CardActionArea>
