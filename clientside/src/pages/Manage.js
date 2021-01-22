34434145c7fe427ac8e335088af71cd3dfe5c7e0
charfweh <affan.ismail2016@gmail.com>
2021-01-22 11:15:51 +0530

    Update: guildcard modifs

--- clientside/src/pages/Manage.js
+++ clientside/src/pages/Manage.js
@@ -3,12 +3,15 @@ import React ,{Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import MutualGuildCard , {InviteGuildCard} from "../chunks/guildCard"
 import NavBarComponent from "../Navbar"
+import {Alert} from "@material-ui/lab"
 class Manage extends Component {
   constructor(props){
     super(props)
     this.state = {
       mutualGuilds : [],
       inviteGuilds : [],
+      login: false,
+      username: "Username"
     }
   }
   componentDidMount(){
@@ -20,35 +23,54 @@ class Manage extends Component {
         })
       }
     })
+
+    axios.get("/authorize/getUserData").then(res=>{
+      console.log("res" + res.data.login)
+      if(res.data.login) {
+        this.setState({
+          login: true,
+          username : res.data.username
+        })
+      }
+    }).catch((err)=>{
+      console.log(err);
+    })
+
   }
+    // Please check REACT component lifecycle to understand this, whenever component mounts itself, this method will be called
+
+  
+
   render() {
     let mutualG = this.state.mutualGuilds.map(g=>{
       console.log(g)
       return(
         <Col sm = "3">
             <MutualGuildCard mutualGuilds = {g}/>
+            <br></br>
         </Col>
       )
     })
+    console.log(mutualG.length)
 
     let inviteG = this.state.inviteGuilds.map(g=>{
       console.log(g)
       return(
         <Col sm = "3">
           <InviteGuildCard inviteGuilds = {g}/>
+          <br></br>
         </Col>
       )
     })
     return (
       <div>
-        <NavBarComponent/>
+        <NavBarComponent username = {this.state.username} isLogin = {this.state.login}/>
         <Container fluid>
           <Row>
-            {mutualG}
+            {mutualG.length != 0 ? mutualG: <Alert severity = "error">Data couldnt be fetched.</Alert>}
           </Row>
-          <br></br>
           <Row>
-            {inviteG}
+            {inviteG.length != 0?  inviteG: <Alert severity = "error">Data couldnt be fetched.</Alert>}
           </Row>
         </Container>
       </div>
