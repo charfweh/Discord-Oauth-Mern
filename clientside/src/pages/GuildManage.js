c17a0f01e2ee653df2a2965a98a2f0382655fc2e
charfweh <affan.ismail2016@gmail.com>
2021-01-26 19:38:51 +0530

    Stable release v1.0

--- clientside/src/pages/GuildManage.js
+++ clientside/src/pages/GuildManage.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 class GuildManage extends Component {
     render() {
         return (
-            <div>
+            <div className = "App">
+                <div className = "App-header">
                 <h1>Guild Manage page</h1>
+                </div>
             </div>
         );
     }
